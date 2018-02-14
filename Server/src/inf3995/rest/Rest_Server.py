e230e1f3cc69d302ecf936979e86f9bc7bd8862a
Charles Hosson <charles.hosson@polymtl.ca>
2018-02-14 17:16:12 -0500

    Bugfix dans la partie REST * Les sessions fonctionnent et sont accessibles publiquement de la classe RestServer * Un peu de refactoring pour que le code soit plus beau

--- Server/src/inf3995/rest/Rest_Server.py
+++ Server/src/inf3995/rest/Rest_Server.py
@@ -17,6 +17,25 @@ class Rest_Server(object):
 	def __init__(self):
 		self.__sessions = {}
 	
+	def get_all_sessions(self):
+		return self.__sessions
+	
+	def GET(self, *args):
+		if len(args) > 1 and args[0] == "config":
+			return self.get_config(cherrypy.request, args[1:len(args)])
+		else:
+			# TODO: Return something when the client asks for "/"? Like a help
+			# page or something?
+			Rest_Server._raise_http_error(404)
+	
+	@cherrypy.tools.accept(media="application/json")
+	@cherrypy.tools.json_in()
+	def POST(self, *args):
+		if len(args) == 2 and args[0] == "users":
+			return self.post_users(cherrypy.request, args[1:len(args)])
+		else:
+			Rest_Server._raise_http_error(404)
+	
 	def get_config(self, request, url):
 		switch = {
 			"basic"       : self.get_config_basic,
@@ -30,17 +49,16 @@ class Rest_Server(object):
 	def get_config_basic(self, request, url):
 		if not self._is_logged_in():
 			Rest_Server._raise_http_error(401)
-		
-		if len(url) == 0:
-			result = {
-				"otherPort" : Program_Options.get_value("server"),
-				"layout"    : Program_Options.get_value("rocket"),
-				"map"       : Program_Options.get_value("map")
-			}
-			cherrypy.response.headers["Content-Type"] = "application/json"
-			return json.dumps(result, indent=2).encode("utf-8")
-		else:
+		if len(url) != 0:
 			Rest_Server._raise_http_error(404)
+		
+		result = {
+			"otherPort" : Program_Options.get_value("server"),
+			"layout"    : Program_Options.get_value("rocket"),
+			"map"       : Program_Options.get_value("map")
+		}
+		cherrypy.response.headers["Content-Type"] = "application/json"
+		return json.dumps(result, indent=2).encode("utf-8")
 	
 	def get_config_rockets(self, request, url):
 		if not self._is_logged_in():
@@ -56,15 +74,14 @@ class Rest_Server(object):
 	def get_config_map(self, request, url):
 		if not self._is_logged_in():
 			Rest_Server._raise_http_error(401)
-		
-		if len(url) == 0:
-			result = {
-				"map" : Program_Options.get_value("map"),
-			}
-			cherrypy.response.headers["Content-Type"] = "application/json"
-			return json.dumps(result, indent=2).encode("utf-8")
-		else:
+		if len(url) != 0:
 			Rest_Server._raise_http_error(404)
+		
+		result = {
+			"map" : Program_Options.get_value("map"),
+		}
+		cherrypy.response.headers["Content-Type"] = "application/json"
+		return json.dumps(result, indent=2).encode("utf-8")
 	
 	def get_config_miscfiles(self, request, url):
 		if not self._is_logged_in():
@@ -78,6 +95,9 @@ class Rest_Server(object):
 			Rest_Server._raise_http_error(404)
 	
 	def get_config_devicetypes(self, *args):
+		if len(url) != 0:
+			Rest_Server._raise_http_error(401)
+		
 		result = {}
 		for i, dev in enumerate(Rest_Server.__POSSIBLE_DEVICES):
 			result["deviceType" + str(i + 1)] = dev
@@ -93,49 +113,39 @@ class Rest_Server(object):
 		return switch.get(url[0], Rest_Server._raise_error_404)(request, url[1:len(url)])
 	
 	def post_users_login(self, request, url):
-		if len(url) == 0:
-			data = request.json
-			if self._is_valid_login_info(data):
-				self.__sessions[cherrypy.session.id] = {}
-				session = self._get_session()
-				session["user"] = data["username"]
-				session["logged"] = True
-				session["ip"] = request.remote.ip
-				session["datetime"] = datetime.datetime.now()
-				session["device"] = self._get_device_from_request(request)
+		if len(url) != 0:
+			Rest_Server._raise_http_error(404)
+		
+		data = request.json
+		if self._is_valid_login_info(data):
+			cherrypy.session["logged"] = True
+			self.__sessions[cherrypy.session.id] = {}
+			session = self.__sessions[cherrypy.session.id]
+			session["user"] = data["username"]
+			session["logged"] = True
+			session["ip"] = request.remote.ip
+			session["datetime"] = datetime.datetime.now()
+			session["device"] = self._get_device_from_request(request)
+		else:
+			Rest_Server._raise_http_error(401)
+	
+	def post_users_logout(self, request, url):
+		if not self._is_logged_in():
+			Rest_Server._raise_http_error(401)
+		if len(url) != 0:
+			Rest_Server._raise_http_error(404)
+		
+		data = request.json
+		if len(data) == 1 and "username" in data:
+			session = self._get_session()
+			if "username" in data and data["username"] == session["user"]:
+				self.__sessions.pop(cherrypy.session.id)
+				cherrypy.session.clear()
+				cherrypy.session.delete()
 			else:
 				Rest_Server._raise_http_error(401)
 		else:
-			Rest_Server._raise_http_error(404)
-	
-	def post_users_logout(self, request, url):
-		if len(url) == 0:
-			data = request.json
-			if len(data) == 1 and "username" in data:
-				if "username" in data and data["username"] == cherrypy.session["user"]:
-					self.__sessions.pop(cherrypy.session.id)
-					cherrypy.session.clear()
-					cherrypy.session.delete()
-				else:
-					Rest_Server._raise_http_error(401)
-			else:
-				Rest_Server._raise_http_error(400)
-		else:
-			Rest_Server._raise_http_error(404)
-	
-	def GET(self, *args):
-		if len(args) > 1 and args[0] == "config":
-			return self.get_config(cherrypy.request, args[1:len(args)])
-		else:
-			Rest_Server._raise_http_error(404)
-	
-	@cherrypy.tools.accept(media="application/json")
-	@cherrypy.tools.json_in()
-	def POST(self, *args):
-		if len(args) == 2 and args[0] == "users":
-			return self.post_users(cherrypy.request, args[1:len(args)])
-		else:
-			Rest_Server._raise_http_error(404)
+			Rest_Server._raise_http_error(401)
 	
 	@staticmethod
 	def _raise_http_error(code):
@@ -146,7 +156,10 @@ class Rest_Server(object):
 		raise Rest_Server._raise_http_error(404)
 	
 	def _is_logged_in(self):
-		return cherrypy.session.get("logged", False)
+		return self._get_session() is not None
+	
+	def _get_session(self):
+		return self.__sessions.get(cherrypy.session.id, None)
 	
 	def _is_valid_login_info(self, data):
 		expected = [{"username" : "foo", "password" : "password1234"},
@@ -161,9 +174,6 @@ class Rest_Server(object):
 				return True
 		return False
 	
-	def _get_session(self):
-		return self.__sessions[cherrypy.session.id]
-	
 	def _get_device_from_request(self, request):
 		data = request.json
 		if "device" in data:
@@ -174,5 +184,4 @@ class Rest_Server(object):
 		else:
 			# TODO: Parse the User-Agent string to try and find the device type.
 			return Rest_Server.__UNKNOWN_DEVICE
-
 
