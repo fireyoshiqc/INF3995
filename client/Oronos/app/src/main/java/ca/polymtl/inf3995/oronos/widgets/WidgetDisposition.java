12ea38cce41208d2c008cc392368344dd94e8dc9
fireyoshiqc <fire_yoshi_qc@hotmail.com>
2018-02-22 09:24:33 -0500

    Cleaned up previous tentative, before merge-rebase.

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/widgets/WidgetDisposition.java
+++
@@ -1,9 +0,0 @@
-package ca.polymtl.inf3995.oronos.widgets;
-
-/**
- * Created by Felix on 13/févr./2018.
- */
-
-public enum WidgetDisposition {
-    RESPONSIVE, ROW, COLUMN, SPECIFIC, TABS, UNIQUE
-}
