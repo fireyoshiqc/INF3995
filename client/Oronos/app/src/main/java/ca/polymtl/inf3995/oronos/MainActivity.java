236c5dc1c2ef8036c756d22404454a6eea59ce6b
Justine Pepin <justine.pepin@hotmail.com>
2018-02-22 10:18:14 -0500

    Remettre le mainActivity à ce qu'il était pour merge JP

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
@@ -9,16 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Tests for socketClient
-        SocketClient socketClient = new SocketClient(GlobalParameters.CLIENT_ADDRESS, GlobalParameters.CLIENT_PORT);
-        FakeServer server = new FakeServer(GlobalParameters.CLIENT_ADDRESS, GlobalParameters.CLIENT_PORT);
-        server.send("Testing if client can be reached form server through udp");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Client received " + socketClient.numMessagesReceived(0) + " messages.");
     }
 }
