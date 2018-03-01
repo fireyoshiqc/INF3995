2d32a58a1df5594d0c0cb40accff30d09e9bcc7f
FabriceCh <fabrice.charbonneau@hotmail.com>
2018-03-01 14:35:58 -0500

    hamburger menu, changement de theme pour overwrite actionbar, template activity class par dessus mainActivity

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
@@ -1,6 +1,5 @@
 package ca.polymtl.inf3995.oronos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.net.CookieHandler;
@@ -8,19 +7,16 @@ import java.net.CookieManager;
 
 import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends DrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
+        super.onCreateDrawer();
         CookieHandler.setDefault(new CookieManager());
 
-        setContentView(R.layout.activity_main);
-
         Timber.plant(new LogTree());
-
 
     }
 
