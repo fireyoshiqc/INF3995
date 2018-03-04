ea788422fbd02e70eee7e16b72f832bf24b7763a
Justine Pepin <justine.pepin@hotmail.com>
2018-03-04 17:08:26 -0500

    Toolbar avec icône pour future grid.

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/MainActivity.java
@@ -1,7 +1,12 @@
 package ca.polymtl.inf3995.oronos;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.net.CookieHandler;
 import java.net.CookieManager;
@@ -20,13 +25,18 @@ import ca.polymtl.inf3995.oronos.parser.UnsupportedContainerWidgetException;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         CookieHandler.setDefault(new CookieManager());
+
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("ORONOS");
+
 
         OronosXmlParser parser = new OronosXmlParser(getWindow().getContext());
         try {
@@ -39,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 ((DualVWidget)tab.getContents()).buildContents();
             }
             tabtest.buildTabs(getWindow().getContext());
-            setContentView(tabtest.getView());
+            ConstraintLayout dataLayout = findViewById(R.id.data_layout);
+            dataLayout.addView(tabtest.getView(), -1);
+            //setContentView(tabtest.getView());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,11 +61,34 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        //setContentView(R.layout.activity_main);
-
         Timber.plant(new LogTree());
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_display_grid) {
+            Toast.makeText(
+                    MainActivity.this,
+                    "Action Display Grid clicked",
+                    Toast.LENGTH_LONG
+            ).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
