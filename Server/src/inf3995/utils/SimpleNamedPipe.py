eb3c33434b0ab56dfc41ec6731c86f32a499e17b
Charles Hosson <charles.hosson@polymtl.ca>
2018-03-13 19:01:13 -0400

    Port Linux des named pipes

--- Server/src/inf3995/utils/SimpleNamedPipe.py
+++ Server/src/inf3995/utils/SimpleNamedPipe.py
@@ -2,11 +2,13 @@
 
 import platform
 import sys
-import os
 import enum
 
 if platform.system() == "Windows":
 	import wpipe
+else:
+	import os
+	import errno
 
 
 @enum.unique
@@ -19,10 +21,11 @@ class SimpleNamedPipeServer(object):
 	def __init__(self, name, mode):
 		self.__pipe = None
 		self.__mode = mode
+		self.__name = name
 		if mode == PipeMode.WRITER:
-			self.__open_writer_pipe(name)
+			self.__open_writer_pipe()
 		elif mode == PipeMode.READER:
-			self.__open_reader_pipe(name)
+			self.__open_reader_pipe()
 		else:
 			raise RuntimeError("Unknown pipe open mode")
 	
@@ -33,7 +36,7 @@ class SimpleNamedPipeServer(object):
 		self.__write_to_clients(data)
 	
 	def read(self):
-		if self.__mode == PipeMode.READER:
+		if self.__mode == PipeMode.WRITER:
 			raise RuntimeError("Cannot read from writer pipe")
 		
 		return self.__read_from_clients()
@@ -42,11 +45,11 @@ class SimpleNamedPipeServer(object):
 		self.__close_pipe()
 	
 	if platform.system() == "Windows":
-		def __open_writer_pipe(self, name):
-			self.__pipe = wpipe.Server(name, wpipe.Mode.Writer)
+		def __open_writer_pipe(self):
+			self.__pipe = wpipe.Server(self.__name, wpipe.Mode.Writer)
 		
-		def __open_reader_pipe(self, name):
-			self.__pipe = wpipe.Server(name, wpipe.Mode.Reader)
+		def __open_reader_pipe(self):
+			self.__pipe = wpipe.Server(self.__name, wpipe.Mode.Reader)
 		
 		def __write_to_clients(self, data):
 			for client in self.__pipe:
@@ -63,30 +66,56 @@ class SimpleNamedPipeServer(object):
 		def __close_pipe(self):
 			self.__pipe.shutdown()
 	else:
-		def __open_writer_pipe(self, name):
-			raise NotImplementedError()
+		def __open_writer_pipe(self):
+			self.__unlink_pipe()
+			os.mkfifo(self.__name, 0o666)
+			self.__pipe = os.open(self.__name, os.O_WRONLY)
 		
-		def __open_reader_pipe(self, name):
-			raise NotImplementedError()
+		def __open_reader_pipe(self):
+			self.__unlink_pipe()
+			os.mkfifo(self.__name, 0o666)
+			self.__pipe = os.open(self.__name, os.O_RDONLY | os.O_NONBLOCK)
 		
-		def __write_to_clients(self, name):
-			raise NotImplementedError()
+		def __write_to_clients(self, data):
+			os.write(self.__pipe, data)
 		
 		def __read_from_clients(self):
-			raise NotImplementedError()
+			try:
+				msg = os.read(self.__pipe, 4096)
+				if len(msg) == 0:
+					return [None]
+				return [msg]
+			except OSError as e:
+				if e.errno == errno.EAGAIN or e.errno == errno.EWOULDBLOCK:
+					return [None]
+				else:
+					raise
+			except:
+				raise
 		
 		def __close_pipe(self):
-			raise NotImplementedError()
+			os.close(self.__pipe)
+			self.__unlink_pipe()
+		
+		def __unlink_pipe(self):
+			try:
+				os.unlink(self.__name)
+			except FileNotFoundError:
+				pass
+			except:
+				raise
+
 
 
 class SimpleNamedPipeClient(object):
 	def __init__(self, name, mode):
 		self.__pipe = None
 		self.__mode = mode
+		self.__name = name
 		if mode == PipeMode.WRITER:
-			self.__open_writer_pipe(name)
+			self.__open_writer_pipe()
 		elif mode == PipeMode.READER:
-			self.__open_reader_pipe(name)
+			self.__open_reader_pipe()
 		else:
 			raise RuntimeError("Unknown pipe open mode")
 	
@@ -103,11 +132,11 @@ class SimpleNamedPipeClient(object):
 		return self.__read()
 	
 	if platform.system() == "Windows":
-		def __open_writer_pipe(self, name):
-			self.__pipe = wpipe.Client(name, wpipe.Mode.Writer)
+		def __open_writer_pipe(self):
+			self.__pipe = wpipe.Client(self.__name, wpipe.Mode.Writer)
 		
-		def __open_reader_pipe(self, name):
-			self.__pipe = wpipe.Client(name, wpipe.Mode.Reader)
+		def __open_reader_pipe(self):
+			self.__pipe = wpipe.Client(self.__name, wpipe.Mode.Reader)
 		
 		def __write(self, data):
 			self.__pipe.write(data)
@@ -115,16 +144,27 @@ class SimpleNamedPipeClient(object):
 		def __read(self):
 			return self.__pipe.read()
 	else:
-		def __open_writer_pipe(self, name):
-			raise NotImplementedError()
+		def __open_writer_pipe(self):
+			self.__pipe = os.open(self.__name, os.O_WRONLY)
 		
-		def __open_reader_pipe(self, name):
-			raise NotImplementedError()
+		def __open_reader_pipe(self):
+			self.__pipe = os.open(self.__name, os.O_RDONLY | os.O_NONBLOCK)
 		
-		def __write_to_clients(self, name):
-			raise NotImplementedError()
+		def __write(self, data):
+			os.write(self.__pipe, data)
 		
-		def __read_from_clients(self):
-			raise NotImplementedError()
+		def __read(self):
+			try:
+				msg = os.read(self.__pipe, 4096)
+				if len(msg) == 0:
+					raise RuntimeError("Pipe closed")
+				return [msg]
+			except OSError as e:
+				if e.errno == errno.EAGAIN or e.errno == errno.EWOULDBLOCK:
+					return [None]
+				else:
+					raise
+			except:
+				raise
 	
 
