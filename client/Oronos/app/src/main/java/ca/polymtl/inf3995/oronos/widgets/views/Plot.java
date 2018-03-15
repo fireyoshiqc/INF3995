d48e3f4777d523753bb60bcc850ceba0c27bd232
FabriceCh <fabrice.charbonneau@hotmail.com>
2018-03-15 12:35:42 -0400

    multiple datasets, structural Data class for easier use when implmentation with data dispatcher happens

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/widgets/views/Plot.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/widgets/views/Plot.java
@@ -2,6 +2,8 @@ package ca.polymtl.inf3995.oronos.widgets.views;
 
 import android.content.Context;
 
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,14 +30,75 @@ public class Plot extends AbstractWidgetContainer<CAN> {
     private final String name;
     private final String unit;
     private final String axis;
+    private final List<CAN> canList;
 
-    private final int CHART_HEIGHT = 400; //match_parent is not working like we want it to for the height
+    private List<Data> dataList;
+
+    private int seconds;
 
     private Context context;
 
     private LinearLayout containerLayout;
-    private TextView textView;
+    private LinearLayout horizontalLayout;
+    private TextView titleView;
+    private TextView axisView;
     private LineChart chart;
+
+    //Class containing information about a certain data from the graph, allowing to update it
+    private class Data {
+
+        private LineDataSet lineDataSet;
+
+        Data(String nameID, int color, List<Entry> entryList){
+            this.lineDataSet = new LineDataSet(entryList, nameID);
+            this.lineDataSet.setColor(color);
+            this.lineDataSet.setCircleColor(color);
+        }
+
+        public LineDataSet getDataSet(){
+            return this.lineDataSet;
+        }
+
+    }
+
+    private void initializeDataList(){
+        this.dataList = new ArrayList<Data>();
+        int[] colors = {Color.RED,Color.GREEN,Color.BLUE, Color.YELLOW, Color.CYAN, Color.MAGENTA};
+        //TODO: get real data
+
+        for (int can = 0; can < canList.size(); can++){
+            List<Entry> entries = new ArrayList<Entry>();
+            for (int i = 0; i < 10; i++) {
+                // turn data into Entry objects
+                entries.add(new Entry(i+can, i));
+            }
+            dataList.add(new Data(canList.get(can).getId(), colors[can], entries));
+        }
+    }
+
+    private void initializeViews(){
+        createTitle();
+        createAxis();
+        this.chart.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
+        this.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        this.containerLayout = new LinearLayout(context);
+        this.containerLayout.setOrientation(LinearLayout.VERTICAL);
+        this.containerLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        this.containerLayout.addView(this.titleView);
+
+        this.horizontalLayout = new LinearLayout(context);
+        this.horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+        this.horizontalLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+
+        this.horizontalLayout.addView(this.axisView);
+        this.horizontalLayout.addView(this.chart);
+
+        this.containerLayout.addView(this.horizontalLayout);
+        addView(this.containerLayout);
+    }
 
     Plot(Context context, String name, String unit, String axis, List<CAN> list) {
 
@@ -42,57 +106,70 @@ public class Plot extends AbstractWidgetContainer<CAN> {
         this.name = name;
         this.unit = unit;
         this.axis = axis;
+        this.canList = list;
         this.context = context;
+        this.seconds = 60;
+
+        initializeDataList();
 
         generatePlot();
-        setGenericSettings();
+        setGenericPlotSettings();
 
-        //TODO: linear vertical layout, add title textview on top of the chart
-        containerLayout = new LinearLayout(context);
+        initializeViews();
 
-        addView(chart);
+    }
 
+    private void createAxis(){
+
+        this.axisView = new TextView(context);
+        this.axisView.setText(axis + " (" + unit + ")");
+        this.axisView.setRotation(-90);
+        this.axisView.setGravity(Gravity.CENTER_HORIZONTAL);
+        this.axisView.setGravity(Gravity.CENTER_VERTICAL);
+        this.axisView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        ));
 
     }
 
     private void createTitle(){
 
-        this.textView = new TextView(context);
+        this.titleView = new TextView(context);
 
-        this.textView.setText(name);
-        this.textView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
+        if(name == ""){
+            this.titleView.setText("this graph has no name");
+        } else {
+            this.titleView.setText(name);
+        }
+        this.titleView.setGravity(Gravity.CENTER_HORIZONTAL);
+        this.titleView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT
         ));
-
 
     }
 
     private void generatePlot() {
 
         this.chart = new LineChart(context);
-        List<Entry> entries = new ArrayList<Entry>();
 
-        for (int i = 0; i < 10; i++) {
-            // turn data into Entry objects
-            entries.add(new Entry(i, i));
+        LineData lineData = new LineData();
+        List<ILineDataSet> lines = new ArrayList<ILineDataSet> ();
+        for (Data data : dataList){
+            lines.add(data.getDataSet());
         }
-        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-
-        LineData lineData = new LineData(dataSet);
-        this.chart.setData(lineData);
+        String[] xAxis = new String[] {"0", "1", "2", "3", "4", "5", "6", "8", "9"};
+        this.chart.setData(new LineData(lines));
         this.chart.invalidate(); // refresh
 
-        //For each datasets in graph, set a different color to them
 
     }
 
-    private void setGenericSettings() {
+    private void setGenericPlotSettings() {
+        //no interaction
+        this.chart.setTouchEnabled(false);
 
-        this.chart.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                CHART_HEIGHT
-        ));
         this.chart.setNoDataText("No data");
         Description desc = new Description();
         desc.setText("Graph description");
