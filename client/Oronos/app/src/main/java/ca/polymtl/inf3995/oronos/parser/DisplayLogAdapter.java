b62d02623039b8d67b86a6ef695b70b9b4df9ac2
Justine Pepin <justine.pepin@hotmail.com>
2018-03-21 09:06:21 -0400

    Première ébauche réaliste du DisplayLogAdapter

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/parser/DisplayLogAdapter.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/parser/DisplayLogAdapter.java
@@ -1,110 +1,75 @@
 package ca.polymtl.inf3995.oronos.parser;
 
-// Nice link for what you want : https://blog.stylingandroid.com/scrolling-recyclerview-part-1/
-
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import ca.polymtl.inf3995.oronos.R;
+import ca.polymtl.inf3995.oronos.GlobalParameters;
 
-public class DisplayLogAdapter extends RecyclerView.Adapter<DisplayLogAdapter.CANContainer> {
+public class DisplayLogAdapter extends RecyclerView.Adapter<DisplayLogAdapter.ViewHolder> {
 
     private Context context;
     private List<String> csvMsgs;
+    private int beginningOfListIndex;
 
-    public DisplayLogAdapter(Context context) { this.context = context; }
+    public DisplayLogAdapter(Context context) {
+        this.context = context;
+        csvMsgs = new ArrayList<>();
+        beginningOfListIndex = 0;
+    }
 
-    @Override
-    public CANContainer onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = null;
-        if (this.getItemCount() > maxLargeItems) {
-            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.can_data_small, parent, false);
+
+    public void addCSVMsg(String csvMsg) {
+        if (csvMsgs.size() < GlobalParameters.LIMIT_OF_N_MSG) {
+            csvMsgs.add(csvMsg);
         } else {
-            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.can_data_large, parent, false);
+            csvMsgs.set(beginningOfListIndex, csvMsg);
+            beginningOfListIndex = beginningOfListIndex + 1 % GlobalParameters.LIMIT_OF_N_MSG;
         }
-
-        return new CANContainer(itemView);
     }
 
     @Override
-    public void onBindViewHolder(CANContainer holder, int position) {
-        CAN can = canTags.get(position);
-        holder.name.setText(can.getName());
-        holder.canid.setText(can.getId());
-        double dummyData = 0.000000;
-        // TODO: Use notify event to send formatted data into the holder.
-        String toDisplay = "" + dummyData;
-        if (can.getDisplay() != null) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(
+                android.R.layout.simple_list_item_1,
+                parent,
+                false
+        );
+        return ViewHolder.newInstance(view);
+    }
 
-            if (can.getChiffresSign() != null) {
-                String signFormat = "%." + can.getChiffresSign() + "f";
-                toDisplay = String.format(signFormat, dummyData);
-            }
-            holder.data.setText(toDisplay);
-            String[] dataSplit = can.getDisplay().split(" ");
-            if (dataSplit.length == 2) {
-                holder.unit.setText(dataSplit[1]);
-            } else {
-                holder.unit.setText("");
-            }
-        } else {
-            // TODO: Use customUpdate to generate the data to display.
-            if (can.getChiffresSign() != null) {
-                String signFormat = "%." + can.getChiffresSign() + "f";
-                toDisplay = String.format(signFormat, dummyData);
-            }
-            holder.data.setText(toDisplay);
-            holder.unit.setText("CST");
-        }
-
-        // TODO: Change holder appearance according to minAcceptable and maxAcceptable.
-        try {
-            if (can.getMinAcceptable() != null && can.getMaxAcceptable() != null) {
-                if (dummyData < Double.parseDouble(can.getMinAcceptable())
-                        || dummyData > Double.parseDouble(can.getMaxAcceptable())) {
-                    holder.itemView.setBackgroundResource(R.drawable.can_data_large_border_red);
-                    holder.data.setTextColor(0xFFCC0000);
-                } else {
-                    holder.itemView.setBackgroundResource(R.drawable.can_data_large_border_green);
-                    holder.data.setTextColor(Color.BLACK);
-                }
-            }
-
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-
-
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String text = csvMsgs.get(position + beginningOfListIndex % GlobalParameters.LIMIT_OF_N_MSG);
+        holder.setText(text);
     }
 
     @Override
     public int getItemCount() {
-        return canTags.size();
+        return csvMsgs.size();
     }
 
-    class CANContainer extends RecyclerView.ViewHolder {
-        public TextView name;
-        public TextView canid;
-        public TextView data;
-        public TextView unit;
+    public static final class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
 
-        public CANContainer(View view) {
-            super(view);
-            name = view.findViewById(R.id.name);
-            canid = view.findViewById(R.id.canid);
-            data = view.findViewById(R.id.data);
-            unit = view.findViewById(R.id.unit_and_warning);
+        private static ViewHolder newInstance(View itemView) {
+            TextView textView = itemView.findViewById(android.R.id.text1);
+            return new ViewHolder(itemView, textView);
         }
 
+        private ViewHolder(View itemView, TextView textView) {
+            super(itemView);
+            this.textView = textView;
+        }
+
+        public void setText(String text) {
+            textView.setText(text);
+        }
     }
-
-
 }
-
