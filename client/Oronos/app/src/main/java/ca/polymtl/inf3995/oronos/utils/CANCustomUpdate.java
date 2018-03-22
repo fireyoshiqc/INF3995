fcf6cc3cc84d0f4603b27609aa06c7c49f4c8c2c
Félix Boulet <fire_yoshi_qc@hotmail.com>
2018-03-22 17:39:26 -0400

    Fini les customUpdates

--- client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/utils/CANCustomUpdate.java
+++ client/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/utils/CANCustomUpdate.java
@@ -1,6 +1,10 @@
 package ca.polymtl.inf3995.oronos.utils;
 
-import android.content.BroadcastReceiver;
+import android.annotation.SuppressLint;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 
 import ca.polymtl.inf3995.oronos.services.BroadcastMessage;
 
@@ -10,24 +14,40 @@ import ca.polymtl.inf3995.oronos.services.BroadcastMessage;
 
 // TODO: FINISH THIS USING CustomUpdate.py as a base
 
+@SuppressLint("DefaultLocale")
 public class CANCustomUpdate {
-    public static String armingStatusUpdate(BroadcastMessage msg) {
+
+    private static HashMap<String, Double> altMax = new HashMap<>();
+    private static HashMap<String, Double> lastRampAlt = new HashMap<>();
+
+
+    // REFLECTION WOOOOOO
+    public static String format(String customUpdate, BroadcastMessage msg) {
+
+        try {
+            Method method = CANCustomUpdate.class.getDeclaredMethod(customUpdate, BroadcastMessage.class);
+            method.setAccessible(true);
+            return (String) method.invoke(null, msg);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return "WRONG_UPDATE";
+        }
+    }
+
+    private static String armingStatusUpdate(BroadcastMessage msg) {
         int armingStatus = msg.getData1().intValue();
         if (armingStatus == 0) {
             return "DISARMED";
-        }
-        else if (armingStatus == 1) {
+        } else if (armingStatus == 1) {
             return "ARMED";
-        }
-        else {
+        } else {
             return "UNKNOWN";
         }
     }
 
-    public static String admStateUpdate(BroadcastMessage msg) {
+    private static String admStateUpdate(BroadcastMessage msg) {
         int admState = msg.getData1().intValue();
 
-        switch(admState) {
+        switch (admState) {
             case 0:
                 return "INIT";
             case 1:
@@ -47,18 +67,107 @@ public class CANCustomUpdate {
         }
     }
 
-    public static String pressToAlt(BroadcastMessage msg) {
-        pascalToFeet(msg.getData1().doubleValue());
-        return null;
+    private static String pressToAlt(BroadcastMessage msg) {
+        double altFeet;
+        if (lastRampAlt.containsKey(msg.getModuleSource().name() + msg.getNoSerieSource())) {
+            altFeet = pascalToFeet(msg.getData1().doubleValue()) -
+                    lastRampAlt.get(msg.getModuleSource().name() + msg.getNoSerieSource());
+        } else {
+            altFeet = 0;
+        }
+        return String.format("%.0f", altFeet)+" ft";
 
+    }
+
+    private static String rampAlt(BroadcastMessage msg) {
+        double rampAltFt = metersToFeet(msg.getData1().doubleValue());
+        lastRampAlt.put(msg.getModuleSource().name() + msg.getNoSerieSource(), rampAltFt);
+        if (msg.getModuleSource() == ModuleType.ADM && msg.getNoSerieSource() == 1) {
+            lastRampAlt.put(ModuleType.ADIRM.name() + 0, rampAltFt);
+        }
+        return String.format("%.0f", rampAltFt)+" ft";
+    }
+
+    private static String apogeeDetect(BroadcastMessage msg) {
+        String key = msg.getModuleSource().name() + msg.getNoSerieSource();
+        if (!altMax.containsKey(key)) {
+            altMax.put(msg.getModuleSource().name() + msg.getNoSerieSource(), Double.NEGATIVE_INFINITY);
+        }
+
+        double altFeet = Double.NEGATIVE_INFINITY;
+
+        if (lastRampAlt.containsKey(key)) {
+            altFeet = pascalToFeet(msg.getData1().doubleValue()) - lastRampAlt.get(key);
+        }
+
+        if (altFeet > altMax.get(key)) {
+            altMax.put(key, altFeet);
+        }
+        return String.format("%.0f", altMax.get(key))+" ft";
+    }
+
+    // oneWire needs an address that we don't have.
+
+    private static String admBWVoltsToOhmsString(BroadcastMessage msg) {
+        double conv = msg.getData1().doubleValue() * 3.5 / 1000.0;
+        if (conv < 11.0) {
+            conv = Double.POSITIVE_INFINITY;
+        }
+        return String.format("%.1f", conv)+" Ω";
+    }
+
+    private static String meterToFoot(BroadcastMessage msg) {
+        return String.format("%.0f", msg.getData1().doubleValue() * 3.28084)+" ft";
+    }
+
+    private static String footToMeter(BroadcastMessage msg) {
+        return String.format("%.0f", msg.getData1().doubleValue() * 0.3048)+" m";
+    }
+
+    private static String SDSpaceLeft(BroadcastMessage msg) {
+        // Remaining space is sent as kB
+        double data = msg.getData1().doubleValue();
+        if (data < 0) {
+            return "ERROR";
+        }
+        else if (data < 10*1024) {
+            return String.format("%.0f", data) + " kB";
+        }
+        else if (data < 1024*1024) {
+            return String.format("%.2f", data/1024) + " MB";
+        }
+        else {
+            return String.format("%.2f", data/(1024*1024)) + " GB";
+        }
+    }
+
+    private static String SDBytesWritten(BroadcastMessage msg) {
+        // Written space is sent as bytes
+        double data = msg.getData1().doubleValue();
+        if (data < 0) {
+            return "ERROR";
+        }
+        else if (data < 1024*1024) {
+            return String.format("%.0f", data/1024) + " kB";
+        }
+        else if (data < 1024*1024*1024) {
+            return String.format("%.2f", data/(1024*1024)) + " MB";
+        }
+        else {
+            return String.format("%.2f", data/(1024*1024*1024)) + " GB";
+        }
     }
 
     private static double pascalToFeet(double pascal) {
         // Sea level pressure : 1013.25 kPa
         // Sea level temp : 288.15 K
-        double meters = (((Math.log10(pascal/1013.25)/Math.log(5.255))*288.15)-1)/-0.0065;
+        double meters = (((Math.log10(pascal / 1013.25) / Math.log(5.255)) * 288.15) - 1) / -0.0065;
 
         // 1 meter = 3.28084 ft
+        return metersToFeet(meters);
+    }
+
+    private static double metersToFeet(double meters) {
         return meters * 3.28084;
     }
 }
