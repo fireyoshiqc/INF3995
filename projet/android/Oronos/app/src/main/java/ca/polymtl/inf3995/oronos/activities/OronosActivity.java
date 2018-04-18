11c5798a80f3ebb0f1e1d67ba986258165780d61
Félix Boulet <fire_yoshi_qc@hotmail.com>
2018-04-17 23:43:08 -0400

    Cleanup du client et bugfix de permissions

--- projet/android/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/activities/OronosActivity.java
+++ projet/android/Oronos/app/src/main/java/ca/polymtl/inf3995/oronos/activities/OronosActivity.java
@@ -145,6 +145,7 @@ public class OronosActivity extends AppCompatActivity {
                     warningBar.show();
                 }
             }
+            break;
             case FindMe.GPS_PERMISSION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
@@ -160,6 +161,7 @@ public class OronosActivity extends AppCompatActivity {
                     }
                 }
             }
+            break;
         }
     }
 
